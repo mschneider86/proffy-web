f5559d3d50611a1ca2b8b8dc45af22b655e46755
Matheus Schneider <matheus.ssm@gmail.com>
2020-08-03 14:08:11 -0300

    Added TeacherList page

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+  return (
+    <article className='teacher-item'>
+      <header>
+        <img src='' alt='Matheus' />
+        <div>
+          <strong>Matheus Schneider</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada <br />
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências
+      </p>
+
+      <footer>
+        <p>Preço/hora</p>
+        <strong>R$ 80,00</strong>
+        <button type='button'>
+          <img src={whatsappIcon} alt='Whatsapp' />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;

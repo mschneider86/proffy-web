0b6c59236602067f266fd4bfabfab15645590f15
Matheus Schneider <matheus.ssm@gmail.com>
2020-08-03 11:56:31 -0300

    added navigation

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -25,15 +26,15 @@ function Landing() {
         />
 
         <div className='buttons-container'>
-          <a href='#' className='study'>
+          <Link to='/study' className='study'>
             <img src={studyIcon} alt='Estudar' />
             Estudar
-          </a>
+          </Link>
 
-          <a href='#' className='teach'>
+          <Link to='/teach' className='teach'>
             <img src={teachIcon} alt='Dar aulas' />
             Dar aulas
-          </a>
+          </Link>
         </div>
 
         <span className='total-connections'>

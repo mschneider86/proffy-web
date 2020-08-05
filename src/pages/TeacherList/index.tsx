27cf31c1c3534819184454bcc34372e504f935dd
Matheus Schneider <matheus.ssm@gmail.com>
2020-08-05 16:58:18 -0300

    added initial teacherform structure & styling

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,26 +3,18 @@ import React from 'react';
 import './styles.css';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 function TeacherList() {
   return (
     <div id='page-teacher-list' className='container'>
       <PageHeader title='Estes são os proffys disponíveis.'>
         <form id='search-teachers'>
-          <div className='input-block'>
-            <label htmlFor='subject'>Matéria</label>
-            <input type='text' id='subject' />
-          </div>
+          <Input name='subject' label='Matéria' />
 
-          <div className='input-block'>
-            <label htmlFor='week_day'>Dia da semana</label>
-            <input type='text' id='week_day' />
-          </div>
+          <Input name='week_day' label='Dia da semana' />
 
-          <div className='input-block'>
-            <label htmlFor='time'>Hora</label>
-            <input type='text' id='time' />
-          </div>
+          <Input name='time' label='Hora' />
         </form>
       </PageHeader>
 
